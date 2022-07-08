57b78f5639a54f5ae69a5a0b0102f5360152775e
Patel <poojan-bharatbhai.patel.047@my.csun.edu>
2022-07-08 14:07:07 -0700

    component file created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { Sidebar } from "./components/Sidebar";
+import { Feed } from "./components/Feed";
+import { Rightbar } from "./components/Rightbar";
 
 function App() {
+
   return (
-    <div >
-        Hello 
+    <div>
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
     </div>
-  );
+  )
+
 }
 
 export default App;
